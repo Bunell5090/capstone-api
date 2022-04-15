3ab0c2d1198d46a04bb7c45bd2561fc04c5f2615
Bunell5090 <Bunell5090.com>
2022-04-14 21:15:01 -0500

    Update routes file with CRUD routes for all tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  #appts
+  get "/appts" => "appts#index"
+  get "/appts/:id" => "appts#show"
+  post "/appts/" => "appts#create"
+  patch "/appts/:id" => "appts#update"
+  delete "/appts/:id" => "appts#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #customers
+  get "/customers" => "customers#index"
+  get "/customers/:id" => "customers#show"
+  post "/customers/" => "customers#create"
+  patch "/customers/:id" => "customers#update"
+  delete "/customers/:id" => "customers#destroy"
+
+  #pets
+  get "/pets" => "pets#index"
+  get "/pets/:id" => "pets#show"
+  post "/pets/" => "pets#create"
+  patch "/pets/:id" => "pets#update"
+  delete "/pets/:id" => "pets#destroy"
+
+  #users
+  get "/users" => "users#index"
+  get "/users/:id" => "users#show"
+  post "/users/" => "users#create"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
 end
