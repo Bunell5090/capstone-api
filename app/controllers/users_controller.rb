94d25542ccd9e0a6f3e1f86100ae6e3e8d9a33de
Bunell5090 <Bunell5090.com>
2022-04-14 21:19:50 -0500

    Added Create function to Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.new(
+      name: params[:name],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+
 end
