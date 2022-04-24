cddf1a64e33d1a2167de354a92f1f76e461d17b2
Bunell5090 <Bunell5090.com>
2022-04-24 09:39:04 -0500

    Added CRUD functions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,24 @@ class UsersController < ApplicationController
   end
 
   def update
+    id = params[:id]
+    user = User.find_by(id: id)
+    user.user_name = params["user_name"] || user.user_name
+    user.first_name = params["first_name"] || user.first_name
+    user.last_name = params["last_name"] || user.last_name
+    user.password_digest = params["password_digest"] || user.password_digest
+    if user.save #happy path
+      render json: user
+    else #sad path
+      render json: {error_messages: user.errors.full_messages}, status: 422
+    end
+  end
+
+  def destroy
+    user_id = params["id"]
+    user = User.find_by(id: User_id)
+    user.destroy
+    render json: {message: "User has been deleted!"}
   end
 
 end
