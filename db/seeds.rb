6dbc8e74e12d300f7c658873c18e7f108e3d257e
Bunell5090 <Bunell5090.com>
2022-05-08 10:06:45 -0500

    Performed seed dump to backup tables in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-    users = User.create([
-      { user_name: "Bunell5090" ,
-        first_name: "Ben" ,
-      last_name: "Unell" ,
-      email: "Bunell5090@test.com" ,
-     password: "password"}])
+Customer.create!([
+  {first_name: "Chani", last_name: "Unell", address: "940 W. Gordon Terrace", city_state: "Chicago, IL", zipcode: "60613"}
+])
+Pet.create!([
+  {name: "Chloe", customer_id: "1", species: "Dog", breed: "Maltipoo", birthday: "06-06-2020", sex: "Female", sterilized: true}
+])
+User.create!([
+  {user_name: "Bunell5090", first_name: "Ben", last_name: "Unell", password_digest: "$2a$12$GRDSZ1pONkRzkMiy6RWuMuweM/.iV1E0aQfR.HjjW9riiWyxp4mGy", position: nil, email: "Bunell5090@test.com"}
+])
