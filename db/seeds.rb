bfb2713a33760a1d90cbf8aabbe1f922cb467cb6
Bunell5090 <Bunell5090@gmail.com>
2022-05-15 09:02:04 -0500

    Successfully added pet from PetNew.vue, & Seed Dumped to Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,15 @@
 # Customer.create!([
-#   {first_name: "Chani", last_name: "Unell", address: "940 W. Gordon Terrace", city_state: "Chicago, IL", zipcode: "60613"}
-# ])
-# Pet.create!([
-#   {name: "Chloe", customer_id: "1", species: "Dog", breed: "Maltipoo", birthday: "06-06-2020", sex: "Female", sterilized: true}
-# ])
-# User.create!([
-#   {user_name: "Bunell5090", first_name: "Ben", last_name: "Unell", password_digest: "$2a$12$GRDSZ1pONkRzkMiy6RWuMuweM/.iV1E0aQfR.HjjW9riiWyxp4mGy", position: nil, email: "Bunell5090@test.com"}
-# ])
-# Customer.create!([
-#   {first_name: "James", last_name: "Smith", address: "1200 E. Devon ave.", city_state: "Chicago, IL", zipcode: "60659"}
-# ])
-# Customer.create!([
+#   {first_name: "Chani", last_name: "Unell", address: "940 W. Gordon Terrace", city_state: "Chicago, IL", zipcode: "60613"},
+#   {first_name: "James", last_name: "Smith", address: "1200 E. Devon ave.", city_state: "Chicago, IL", zipcode: "60659"},
 #   {first_name: "John", last_name: "Connor", address: "243 W. 76th St.", city_state: "Chicago, IL", zipcode: "60660"}
 # ])
 # Pet.create!([
-#   {name: "Pepper", customer_id: "1", species: "Dog", breed: "Rat Terrier", birthday: "01-01-2006", sex: "Male", sterilized: false}
+#   {name: "Chloe", customer_id: "1", species: "Dog", breed: "Maltipoo", birthday: "06-06-2020", gender: "Female", sterilized: true, image: nil},
+#   {name: "Pepper", customer_id: "1", species: "Dog", breed: "Rat Terrier", birthday: "01-01-2006", gender: "Male", sterilized: false, image: nil},
+#   {name: "Wanda", customer_id: "2", species: "Tarantula", breed: "Mexian Red-Kneed", birthday: "04-06-2022", gender: "Female", sterilized: false, image: nil},
+#   {name: "Rachel", customer_id: "2", species: "Skunk", breed: "American Skunk", birthday: "12-01-2021", gender: "Female", sterilized: true, image: nil},
+#   {name: "Waffles", customer_id: "1", species: "Cheetah", breed: "Cheetah", birthday: "2021-01-02", gender: nil, sterilized: nil, image: nil}
 # ])
-# Pet.create!([
-#   {name: "Wanda", customer_id: "2", species: "Tarantula", breed: "Mexian Red-Kneed", birthday: "04-06-2022", sex: "Female", sterilized: false}
-# ])
-# Pet.create!([
-#   {name: "Rachel", customer_id: "2", species: "Skunk", breed: "American Skunk", birthday: "12-01-2021", sex: "Female", sterilized: true}
+# User.create!([
+#   {user_name: "Bunell5090", first_name: "Ben", last_name: "Unell", password_digest: "$2a$12$GRDSZ1pONkRzkMiy6RWuMuweM/.iV1E0aQfR.HjjW9riiWyxp4mGy", position: nil, email: "Bunell5090@test.com"}
 # ])
